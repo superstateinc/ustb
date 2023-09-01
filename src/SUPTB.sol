7cb1a43cfc03c440f44d9a2c4dfe58499cced194
Aryan Bhasin <bhasinaryan@gmail.com>
2023-09-01 15:16:02 -0400

    add tests for permit

--- src/SUPTB.sol
+++ src/SUPTB.sol
@@ -249,7 +249,7 @@ contract SUPTB is ERC20Upgradeable, IERC7246, PausableUpgradeable {
      * @param s Half of the ECDSA signature pair
      */
     function permit(address owner, address spender, uint256 amount, uint256 expiry, uint8 v, bytes32 r, bytes32 s)
-        external
+        external whenNotPaused
     {
         if (block.timestamp >= expiry) revert SignatureExpired();
 
