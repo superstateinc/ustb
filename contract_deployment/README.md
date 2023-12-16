4e430c4b588a4fb563a0692f77b4ca58da585951
Max Wolff <maxcwolff@gmail.com>
2023-12-16 11:56:02 -0800

    upgrade ustb, add scripts

--- contract_deployment/README.md
+++
@@ -1,7 +0,0 @@
-## Testnet Deployments Changelog
-
-| Deploy File Name | Commit Hash | Notes |
-|------------|-------------|--------|
-| goerli.json     | bfe063d     | allowlist rename, burn changes, not including USTB rename | 
-| goerli_old.json | 96de27d     | audit feedback |
-| sepolia.json    | 83e2229     | misc nits, revert self encumber |
---
+++ script/DeployUstbUpgrade.s.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
+import "src/USTB.sol";
+import "src/AllowList.sol";
+
+contract DeployUstbUpgrade is Script {
+    USTB public tokenImplementation;
+    AllowList public permsImplementation;
+
+    function run() external {
+
+        address deployer = vm.addr(vm.envUint("DEPLOYER_PK"));
+        address admin = vm.envAddress("ADMIN_ADDRESS");
+        address allowlist_address = vm.envAddress("ALLOWLIST_PROXY_ADDRESS");
+        AllowList wrappedPerms = AllowList(address(allowlist_address));
+
+        vm.startBroadcast(deployer);
+
+        tokenImplementation = new USTB(admin, wrappedPerms);
+
+        vm.stopBroadcast();
+    }
+}
