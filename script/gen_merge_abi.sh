68fc8a4fc560728847fdf37d0a8ce96c93692568
Jon <pokemonmasterjmw@gmail.com>
2024-06-26 14:37:43 -0400

    chore: change script/gen_merge_abi.sh to support USCC (#50)

--- script/gen_merge_abi.sh
+++ script/gen_merge_abi.sh
@@ -6,6 +6,7 @@ PROXY_FILE="out/TransparentUpgradeableProxy.sol/ITransparentUpgradeableProxy.jso
 
 # only event abi
 jq -s '.[0].abi + .[1].abi' out/USTB.sol/USTB.json $PROXY_FILE > ustbAbi.json
+jq -s '.[0].abi + .[1].abi' out/USCC.sol/USCC.json $PROXY_FILE > usccAbi.json
 jq -s '.[0].abi + .[1].abi' out/AllowList.sol/AllowList.json $PROXY_FILE > allowlistAbi.json
 
-echo "sucessfully generated ustbAbi.json and allowlistAbi.json"
+echo "successfully generated ustbAbi.json, usccAbi.json and allowlistAbi.json"
