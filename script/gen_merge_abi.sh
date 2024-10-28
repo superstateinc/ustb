92baed6cfaa30354a474434eedd66e617a3407c0
Jon <pokemonmasterjmw@gmail.com>
2024-10-28 17:11:37 -0400

    Jon/update script (#64)
    * it lives
    
    * fix
    
    * we did it

--- script/gen_merge_abi.sh
+++ script/gen_merge_abi.sh
@@ -4,9 +4,18 @@ set -exo pipefail
 
 PROXY_FILE="out/TransparentUpgradeableProxy.sol/ITransparentUpgradeableProxy.json"
 
+forge build
+
+forge inspect src/v2/USTBv2.sol:USTBv2 abi > ustb-impl.json
+forge inspect src/v2/USCCv2.sol:USCCv2 abi > uscc-impl.json
+forge inspect src/AllowList.sol:AllowList abi > allowlist-impl.json
+
 # only event abi
-jq -s '.[0].abi + .[1].abi' out/USTB.sol/USTB.json $PROXY_FILE > ustbAbi.json
-jq -s '.[0].abi + .[1].abi' out/USCC.sol/USCC.json $PROXY_FILE > usccAbi.json
-jq -s '.[0].abi + .[1].abi' out/AllowList.sol/AllowList.json $PROXY_FILE > allowlistAbi.json
+jq -s '.[0] + .[1].abi' ustb-impl.json $PROXY_FILE > ustbAbi.json
+jq -s '.[0] + .[1].abi' uscc-impl.json $PROXY_FILE > usccAbi.json
+jq -s '.[0] + .[1].abi' allowlist-impl.json $PROXY_FILE > allowlistAbi.json
 
 echo "successfully generated ustbAbi.json, usccAbi.json and allowlistAbi.json"
+rm ustb-impl.json
+rm uscc-impl.json
+rm allowlist-impl.json
