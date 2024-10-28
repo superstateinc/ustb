5a221e0000f12fb13ff0830d70478629358f8c82
Jon <pokemonmasterjmw@gmail.com>
2024-10-28 16:50:19 -0400

    fix

--- script/gen_merge_abi.sh
+++ script/gen_merge_abi.sh
@@ -9,10 +9,10 @@ FOO3_FILE="out/Ownable2StepUpgradeable.sol/Ownable2StepUpgradeable.json"
 FOO4_FILE="out/OwnableUpgradeable.sol/OwnableUpgradeable.json"
 
 # only event abi
-jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USTBV2.sol/USTBV2.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > ustbAbi.json
-jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USCCV2.sol/USCCV2.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > usccAbi.json
-#jq -s '.[0].abi + .[1].abi' out/USTBV2.sol/USTBV2.json $PROXY_FILE > ustbAbi.json
-#jq -s '.[0].abi + .[1].abi' out/USCCV2.sol/USCCV2.json $PROXY_FILE > usccAbi.json
+jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USTB.sol/USTB.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > ustbAbi.json
+jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USCC.sol/USCC.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > usccAbi.json
+#jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USTBV2.sol/USTBV2.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > ustbAbi.json
+#jq -s '.[0].abi + .[1].abi + .[2].abi + .[3].abi + .[4].abi + .[5].abi' out/USCCV2.sol/USCCV2.json $PROXY_FILE $FOO_FILE $FOO2_FILE $FOO3_FILE $FOO4_FILE > usccAbi.json
 jq -s '.[0].abi + .[1].abi' out/AllowList.sol/AllowList.json $PROXY_FILE > allowlistAbi.json
 
 echo "successfully generated ustbAbi.json, usccAbi.json and allowlistAbi.json"
