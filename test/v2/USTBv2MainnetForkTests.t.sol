1f08aea5f4b2922082f753d25ef94d2f2db09602
Jon <pokemonmasterjmw@gmail.com>
2024-11-04 15:02:00 -0500

    chore: tests for onchain subscriptions3

--- test/v2/USTBv2MainnetForkTests.t.sol
+++ test/v2/USTBv2MainnetForkTests.t.sol
@@ -1,111 +1,111 @@
-//pragma solidity ^0.8.28;
-//
-//import "forge-std/StdUtils.sol";
-//import {Test} from "forge-std/Test.sol";
-//import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/security/PausableUpgradeable.sol";
-//
-//import "openzeppelin-contracts-v4/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-//import "openzeppelin-contracts-v4/contracts/proxy/transparent/ProxyAdmin.sol";
-//import {console} from "forge-std/console.sol";
-//import {SuperstateTokenV2} from "src/v2/SuperstateTokenV2.sol";
-//import {SuperstateTokenV1} from "src/v1/SuperstateTokenV1.sol";
-//import {USTBv2} from "src/v2/USTBv2.sol";
-//import {USTBv1} from "src/v1/USTBv1.sol";
-//import {AllowList} from "src/AllowList.sol";
-//import "test/AllowListV2.sol";
-//import "test/USTBV2.sol";
-//
-//contract USTBv2MainnetForkTest is Test {
-//    ProxyAdmin proxyAdmin;
-//    TransparentUpgradeableProxy permsProxy;
-//    AllowList public perms;
-//    TransparentUpgradeableProxy tokenProxy;
-//    SuperstateTokenV2 public token;
-//    SuperstateTokenV1 public tokenV1;
-//    address adminAddress;
-//    address capturedMainnetAddress = address(0x008B3EeEE3AaAA5AD8F92De038729FC4fe899f75);
-//    USTBv2 tokenImplementation;
-//
-//    address alice = address(10);
-//    address bob = address(11);
-//
-//    function setUp() public virtual {
-//        string memory rpcUrl = vm.envString("RPC_URL");
-//
-//        uint256 mainnetFork = vm.createFork(rpcUrl);
-//        vm.selectFork(mainnetFork);
-//        assertEq(vm.activeFork(), mainnetFork);
-//
-//        adminAddress = address(0x8C7Db8A96d39F76D9f456db23d591C2FDd0e2F8a);
-//
-//        proxyAdmin = ProxyAdmin(address(0xCb8d325C0Af19697B8454481602097f93aa9040F));
-//        perms = AllowList(address(0x42d75C8FdBBF046DF0Fe1Ff388DA16fF99dE8149)); // This points to the AllowList proxy
-//        tokenProxy = TransparentUpgradeableProxy(payable(0x43415eB6ff9DB7E26A15b704e7A3eDCe97d31C4e));
-//        tokenV1 = USTBv1(address(tokenProxy));
-//    }
-//
-//    function doTokenUpgradeFromV1toV2() public {
-//        // assert logic contract is v1
-//        assertEq("1", tokenV1.VERSION());
-//
-//        // Upgrade to v2 contract
-//        tokenImplementation = new USTBv2(adminAddress, perms);
-//        vm.prank(adminAddress);
-//        proxyAdmin.upgrade(ITransparentUpgradeableProxy(address(tokenProxy)), address(tokenImplementation));
-//
-//        // initialize v2 contract
-//        token = USTBv2(address(tokenProxy));
-//        vm.prank(adminAddress);
-//        token.initializeV2();
-//
-//        // assert logic contract is now v2
-//        assertEq("2", token.VERSION());
-//
-//        // assert owner is the new admin
-//        assertEq(adminAddress, token.owner());
-//    }
-//
-//    function testUpgradeWithMainnetFork() public {
-//        assertEq(vm.activeFork(), 0);
-//
-//        // check balance on an address in v1
-//        assertEq(404619420184, tokenV1.balanceOf(capturedMainnetAddress));
-//
-//        // upgrade to v2
-//        doTokenUpgradeFromV1toV2();
-//
-//        // ensure balance state is the same
-//        assertEq(404619420184, token.balanceOf(capturedMainnetAddress));
-//    }
-//
-//    function testUpgradeAndChangeOwner() public {
-//        assertEq(vm.activeFork(), 0);
-//
-//        // upgrade to v2
-//        doTokenUpgradeFromV1toV2();
-//
-//        // change owner to a new admin
-//        address newAdmin = alice;
-//
-//        vm.prank(adminAddress);
-//        token.transferOwnership(newAdmin);
-//
-//        vm.prank(alice);
-//        token.acceptOwnership();
-//
-//        assertEq(alice, token.owner());
-//
-//        uint256 capturedBalance = token.balanceOf(capturedMainnetAddress);
-//
-//        // ensure alice can perform admin functions
-//        uint256 mintAmount = 100;
-//        vm.prank(newAdmin);
-//        token.mint(capturedMainnetAddress, mintAmount);
-//        assertEq(capturedBalance + mintAmount, token.balanceOf(capturedMainnetAddress));
-//
-//        // ensure the prior admin can no longer perform admin functions
-//        vm.prank(adminAddress);
-//        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-//        token.mint(capturedMainnetAddress, mintAmount);
-//    }
-//}
+pragma solidity ^0.8.28;
+
+import "forge-std/StdUtils.sol";
+import {Test} from "forge-std/Test.sol";
+import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "openzeppelin-contracts-v4/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts-v4/contracts/proxy/transparent/ProxyAdmin.sol";
+import {console} from "forge-std/console.sol";
+import {SuperstateTokenV2} from "src/v2/SuperstateTokenV2.sol";
+import {SuperstateTokenV1} from "src/v1/SuperstateTokenV1.sol";
+import {USTBv2} from "src/v2/USTBv2.sol";
+import {USTBv1} from "src/v1/USTBv1.sol";
+import {AllowList} from "src/AllowList.sol";
+import "test/AllowListV2.sol";
+import "test/USTBV2.sol";
+
+contract USTBv2MainnetForkTest is Test {
+    ProxyAdmin proxyAdmin;
+    TransparentUpgradeableProxy permsProxy;
+    AllowList public perms;
+    TransparentUpgradeableProxy tokenProxy;
+    SuperstateTokenV2 public token;
+    SuperstateTokenV1 public tokenV1;
+    address adminAddress;
+    address capturedMainnetAddress = address(0x008B3EeEE3AaAA5AD8F92De038729FC4fe899f75);
+    USTBv2 tokenImplementation;
+
+    address alice = address(10);
+    address bob = address(11);
+
+    function setUp() public virtual {
+        string memory rpcUrl = vm.envString("RPC_URL");
+
+        uint256 mainnetFork = vm.createFork(rpcUrl);
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+
+        adminAddress = address(0x8C7Db8A96d39F76D9f456db23d591C2FDd0e2F8a);
+
+        proxyAdmin = ProxyAdmin(address(0xCb8d325C0Af19697B8454481602097f93aa9040F));
+        perms = AllowList(address(0x42d75C8FdBBF046DF0Fe1Ff388DA16fF99dE8149)); // This points to the AllowList proxy
+        tokenProxy = TransparentUpgradeableProxy(payable(0x43415eB6ff9DB7E26A15b704e7A3eDCe97d31C4e));
+        tokenV1 = USTBv1(address(tokenProxy));
+    }
+
+    function doTokenUpgradeFromV1toV2() public {
+        // assert logic contract is v1
+        assertEq("1", tokenV1.VERSION());
+
+        // Upgrade to v2 contract
+        tokenImplementation = new USTBv2(adminAddress, perms);
+        vm.prank(adminAddress);
+        proxyAdmin.upgrade(ITransparentUpgradeableProxy(address(tokenProxy)), address(tokenImplementation));
+
+        // initialize v2 contract
+        token = USTBv2(address(tokenProxy));
+        vm.prank(adminAddress);
+        token.initializeV2();
+
+        // assert logic contract is now v2
+        assertEq("2", token.VERSION());
+
+        // assert owner is the new admin
+        assertEq(adminAddress, token.owner());
+    }
+
+    function testUpgradeWithMainnetFork() public {
+        assertEq(vm.activeFork(), 0);
+
+        // check balance on an address in v1
+        assertEq(404619420184, tokenV1.balanceOf(capturedMainnetAddress));
+
+        // upgrade to v2
+        doTokenUpgradeFromV1toV2();
+
+        // ensure balance state is the same
+        assertEq(404619420184, token.balanceOf(capturedMainnetAddress));
+    }
+
+    function testUpgradeAndChangeOwner() public {
+        assertEq(vm.activeFork(), 0);
+
+        // upgrade to v2
+        doTokenUpgradeFromV1toV2();
+
+        // change owner to a new admin
+        address newAdmin = alice;
+
+        vm.prank(adminAddress);
+        token.transferOwnership(newAdmin);
+
+        vm.prank(alice);
+        token.acceptOwnership();
+
+        assertEq(alice, token.owner());
+
+        uint256 capturedBalance = token.balanceOf(capturedMainnetAddress);
+
+        // ensure alice can perform admin functions
+        uint256 mintAmount = 100;
+        vm.prank(newAdmin);
+        token.mint(capturedMainnetAddress, mintAmount);
+        assertEq(capturedBalance + mintAmount, token.balanceOf(capturedMainnetAddress));
+
+        // ensure the prior admin can no longer perform admin functions
+        vm.prank(adminAddress);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        token.mint(capturedMainnetAddress, mintAmount);
+    }
+}
--- test/v3/USTBv3MainnetForkTests.t.sol
+++ test/v3/USTBv3MainnetForkTests.t.sol
@@ -1,79 +1,79 @@
-//pragma solidity ^0.8.28;
-//
-//import "forge-std/StdUtils.sol";
-//import {Test} from "forge-std/Test.sol";
-//import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/security/PausableUpgradeable.sol";
-//
-//import "openzeppelin-contracts-v4/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-//import "openzeppelin-contracts-v4/contracts/proxy/transparent/ProxyAdmin.sol";
-//import {console} from "forge-std/console.sol";
-//import {SuperstateToken} from "src/SuperstateToken.sol";
-//import {SuperstateTokenV2} from "src/v2/SuperstateTokenV2.sol";
-//import {USTBv2} from "src/v2/USTBv2.sol";
-//import {USTB} from "src/USTB.sol";
-//import {AllowList} from "src/AllowList.sol";
-//
-//contract USTBv3MainnetForkTest is Test {
-//    ProxyAdmin proxyAdmin;
-//    TransparentUpgradeableProxy permsProxy;
-//    AllowList public perms;
-//    TransparentUpgradeableProxy tokenProxy;
-//    SuperstateToken public token;
-//    SuperstateTokenV2 public tokenV2;
-//    address adminAddress;
-//    address capturedMainnetAddress = address(0x008B3EeEE3AaAA5AD8F92De038729FC4fe899f75);
-//    USTB tokenImplementation;
-//
-//    address alice = address(10);
-//    address bob = address(11);
-//
-//    function setUp() public virtual {
-//        string memory rpcUrl = vm.envString("RPC_URL");
-//
-//        uint256 mainnetFork = vm.createFork(rpcUrl);
-//        vm.selectFork(mainnetFork);
-//        assertEq(vm.activeFork(), mainnetFork);
-//
-//        adminAddress = address(0xad309BB6f13074128b4F23EF9EA2fe8552AfCA83);
-//
-//        proxyAdmin = ProxyAdmin(address(0xCb8d325C0Af19697B8454481602097f93aa9040F));
-//        perms = AllowList(address(0x42d75C8FdBBF046DF0Fe1Ff388DA16fF99dE8149)); // This points to the AllowList proxy
-//        tokenProxy = TransparentUpgradeableProxy(payable(0x43415eB6ff9DB7E26A15b704e7A3eDCe97d31C4e));
-//        tokenV2 = USTBv2(address(tokenProxy));
-//    }
-//
-//    function doTokenUpgradeFromV2toV3() public {
-//        // assert logic contract is v2
-//        assertEq("2", tokenV2.VERSION());
-//
-//        // Upgrade to v3 contract
-//        tokenImplementation = new USTB(perms);
-//        vm.prank(adminAddress);
-//        proxyAdmin.upgrade(ITransparentUpgradeableProxy(address(tokenProxy)), address(tokenImplementation));
-//
-//        // initialize v3 contract
-//        token = USTB(address(tokenProxy));
-//        vm.prank(adminAddress);
-//        token.initializeV2();
-//
-//        // assert logic contract is now v3
-//        assertEq("3", token.VERSION());
-//
-//        // assert owner is the new admin
-//        assertEq(adminAddress, token.owner());
-//    }
-//
-//    // TODO: [FAIL: revert: Ownable: caller is not the owner] testUpgradeWithMainnetFork() (gas: 2666431)
-//    function testUpgradeWithMainnetFork() public {
-//        assertEq(vm.activeFork(), 0);
-//
-//        // check balance on an address in v2
-//        assertEq(404619420184, tokenV2.balanceOf(capturedMainnetAddress));
-//
-//        // upgrade to v3
-//        doTokenUpgradeFromV2toV3();
-//
-//        // ensure balance state is the same
-//        assertEq(404619420184, token.balanceOf(capturedMainnetAddress));
-//    }
-//}
+pragma solidity ^0.8.28;
+
+import "forge-std/StdUtils.sol";
+import {Test} from "forge-std/Test.sol";
+import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "openzeppelin-contracts-v4/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts-v4/contracts/proxy/transparent/ProxyAdmin.sol";
+import {console} from "forge-std/console.sol";
+import {SuperstateToken} from "src/SuperstateToken.sol";
+import {SuperstateTokenV2} from "src/v2/SuperstateTokenV2.sol";
+import {USTBv2} from "src/v2/USTBv2.sol";
+import {USTB} from "src/USTB.sol";
+import {AllowList} from "src/AllowList.sol";
+
+contract USTBv3MainnetForkTest is Test {
+    ProxyAdmin proxyAdmin;
+    TransparentUpgradeableProxy permsProxy;
+    AllowList public perms;
+    TransparentUpgradeableProxy tokenProxy;
+    SuperstateToken public token;
+    SuperstateTokenV2 public tokenV2;
+    address adminAddress;
+    address capturedMainnetAddress = address(0x008B3EeEE3AaAA5AD8F92De038729FC4fe899f75);
+    USTB tokenImplementation;
+
+    address alice = address(10);
+    address bob = address(11);
+
+    function setUp() public virtual {
+        string memory rpcUrl = vm.envString("RPC_URL");
+
+        uint256 mainnetFork = vm.createFork(rpcUrl);
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+
+        adminAddress = address(0xad309BB6f13074128b4F23EF9EA2fe8552AfCA83);
+
+        proxyAdmin = ProxyAdmin(address(0xCb8d325C0Af19697B8454481602097f93aa9040F));
+        perms = AllowList(address(0x42d75C8FdBBF046DF0Fe1Ff388DA16fF99dE8149)); // This points to the AllowList proxy
+        tokenProxy = TransparentUpgradeableProxy(payable(0x43415eB6ff9DB7E26A15b704e7A3eDCe97d31C4e));
+        tokenV2 = USTBv2(address(tokenProxy));
+    }
+
+    function doTokenUpgradeFromV2toV3() public {
+        // assert logic contract is v2
+        assertEq("2", tokenV2.VERSION());
+
+        // Upgrade to v3 contract
+        tokenImplementation = new USTB(perms);
+        vm.prank(adminAddress);
+        proxyAdmin.upgrade(ITransparentUpgradeableProxy(address(tokenProxy)), address(tokenImplementation));
+
+        // initialize v3 contract
+        token = USTB(address(tokenProxy));
+        vm.prank(adminAddress);
+        token.initializeV2();
+
+        // assert logic contract is now v3
+        assertEq("3", token.VERSION());
+
+        // assert owner is the new admin
+        assertEq(adminAddress, token.owner());
+    }
+
+    // TODO: [FAIL: revert: Ownable: caller is not the owner] testUpgradeWithMainnetFork() (gas: 2666431)
+    function testUpgradeWithMainnetFork() public {
+        assertEq(vm.activeFork(), 0);
+
+        // check balance on an address in v2
+        assertEq(404619420184, tokenV2.balanceOf(capturedMainnetAddress));
+
+        // upgrade to v3
+        doTokenUpgradeFromV2toV3();
+
+        // ensure balance state is the same
+        assertEq(404619420184, token.balanceOf(capturedMainnetAddress));
+    }
+}
