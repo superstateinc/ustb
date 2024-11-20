394893951a4da402928b58dcc8bbe4d576f485da
Max Wolff <maxcwolff@gmail.com>
2024-11-20 04:49:40 -0800

    fix script (#81)

--- script/gen_merge_abi.sh
+++ script/gen_merge_abi.sh
@@ -3,19 +3,13 @@
 set -exo pipefail
 
 PROXY_FILE="out/TransparentUpgradeableProxy.sol/ITransparentUpgradeableProxy.json"
+SUPERSTATE_TOKEN_FILE="out/SuperstateToken.sol/SuperstateToken.json"
 
 forge build
 
-forge inspect src/v2/USTBv2.sol:USTBv2 abi > ustb-impl.json
-forge inspect src/v2/USCCv2.sol:USCCv2 abi > uscc-impl.json
-forge inspect src/AllowList.sol:AllowList abi > allowlist-impl.json
-
 # only event abi
-jq -s '.[0] + .[1].abi' ustb-impl.json $PROXY_FILE > ustbAbi.json
-jq -s '.[0] + .[1].abi' uscc-impl.json $PROXY_FILE > usccAbi.json
-jq -s '.[0] + .[1].abi' allowlist-impl.json $PROXY_FILE > allowlistAbi.json
+jq -s '.[0].abi + .[1].abi' $PROXY_FILE $SUPERSTATE_TOKEN_FILE> ustbAbi.json
+jq -s '.[0].abi + .[1].abi' $PROXY_FILE $SUPERSTATE_TOKEN_FILE > usccAbi.json
+jq -s '.[0].abi + .[1].abi' out/AllowList.sol/AllowList.json $PROXY_FILE > allowlistAbi.json
 
 echo "successfully generated ustbAbi.json, usccAbi.json and allowlistAbi.json"
-rm ustb-impl.json
-rm uscc-impl.json
-rm allowlist-impl.json
